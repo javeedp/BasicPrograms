03940926c8dfc01b9866853a0457a8ba69c27de2
javeedp <javeedpatayit@gmail.com>
2023-04-13 14:50:39 +0530

    [javeedp] added QuotientandRemainder program

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -13,7 +13,7 @@ namespace BasicCorePrograms
         {
             Console.WriteLine("Welcome To The Basic Core Program");
             Console.WriteLine("Please Choose One From Below Option: ");
-            Console.WriteLine("1.FlipCoin\n 2.LeapYear\n 3.PowerOfTwo\n 4.HarmonicSeries\n 5.EvenorOdd\n 6.Factor\n 7.LargestNumber");
+            Console.WriteLine("1.FlipCoin\n 2.LeapYear\n 3.PowerOfTwo\n 4.HarmonicSeries\n 5.EvenorOdd\n 6.Factor\n 7.LargestNumber\n 8.FindQuotientAndRemainder");
             int Option = Convert.ToInt32(Console.ReadLine());
             switch (Option)
             {
@@ -37,6 +37,9 @@ namespace BasicCorePrograms
                     break;
                 case 7:
                     LargestNumber.FindTheLargestNumber();
+                    break;
+                case 8:
+                    QuotientandRemainder.FindQuotientAndRemainder();
                     break;
                 default:
                     Console.WriteLine("Please Choose Program With Given Option");
---
+++ BasicCorePrograms/QuotientandRemainder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class QuotientandRemainder
+    {
+        public static void FindQuotientAndRemainder()
+        {
+            int dividend = 60, divisor = 9;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend : {0} Divisor: {1}", dividend, divisor);
+            // Console.WriteLine("Dividend : " + dividend+" Divisor: " + divisor);
+
+            Console.WriteLine("Quotient =  " + quotient);
+            Console.WriteLine("Remainder =  " + remainder);
+        }
+    }
+}
