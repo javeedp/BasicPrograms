21a281605ed2d9bacf2d913a953addd0234ec1c3
javeedp <javeedpatayit@gmail.com>
2023-04-13 13:44:35 +0530

    [javeep] adding leapYear

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,21 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Basic Core Program");
-            FlipCoin.Flip();
+            Console.WriteLine("Please Choose One From Below Option: ");
+            Console.WriteLine("1.FlipCoin\n 2.LeapYear\n");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    FlipCoin.Flip();
+                    break;
+                case 2:
+                    LeapYear.TheLeapYear();
+                    break;
+                default:
+                    Console.WriteLine("Please Choose Program With Given Option");
+                    break;
+            }
         }
     }
 }
